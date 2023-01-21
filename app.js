aa314297bc52f4f073bf6b73dfaa41388b4ecac6
Luka Bozovic <luka.bozovic@amplitudo.me>
2023-01-21 18:18:10 +0100

    Set right code from lecture

--- app.js
+++ app.js
@@ -1,70 +1,116 @@
-const users = [
+const questions = [
     {
-        username : "luka",
-        password : "1234",
-        fullName : "Luka Petrovic"
+        text : "Koji od navedenih odgovora predstavlja ime programskog jezika?",
+        answers : {
+            a : "HTML",
+            b : "JS",
+            c : "CSS"
+        },
+        correctAnswer : "b"
     },
     {
-        username : "marko",
-        password : "1234",
-        fullName : "Marko Markovic"
+        text : "Izbaci uljeza: ",
+        answers : {
+            a : "Laravel",
+            b : "Spring Boot",
+            c : "React"
+        },
+        correctAnswer : "c"
     },
     {
-        username : "marija",
-        password : "1234",
-        fullName : "Marija Markovic"
+        text : "Koji je glavni grad Crne Gore?",
+        answers : {
+            a : "Cetinje",
+            b : "Budva",
+            c : "Podgorica"
+        },
+        correctAnswer : "c"
     }
-];
+]
 
-function getUserInput(){
-    let username = document.getElementById("username-input").value;
-    let password = document.getElementById("password-input").value;
+var duration = 15;
 
-    return {
-        username : username,
-        password : password
-    }
-}
+var contentDiv = document.getElementById('contentDiv');
+var accordionDiv = document.getElementById('accordion');
+var resultDiv = document.getElementById('result');
+var tryAgainButton = document.getElementById('tryAgainButton');
+var timeDiv = document.getElementById("timeDiv");
 
-function login(){
-    
-    let obj = getUserInput();
+var timeLeft = 15;
+function start(){
+    timeDiv.innerHTML = `<h2>${timeLeft}</h2>`;
+    let questionsHtml = [];
 
-    let result = false;
+    questions.forEach((question, index) => {
 
-    users.forEach(function(user){
-        if(user.username === obj.username && user.password === obj.password)
-            result = true;
+        let answersHtml = [];
+
+        for(let letter in question.answers){
+            let answerHtml = `<div>
+                                <label>
+                                        <input type="radio" name="question${index}" value="${letter}"> ${letter} : ${question.answers[letter]}
+                                </label>
+                              </div>
+            `;
+            answersHtml.push(answerHtml);
+        }
+        questionsHtml.push(`<div class="accordion-item">
+                                 <h2 class="accordion-header" id="flush-heading${index}">
+                                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapse${index}" aria-expanded="false" aria-controls="flush-collapseOne">
+                                        ${question.text}
+                                    </button>
+                                 </h2>
+                                 
+                                <div id="flush-collapse${index}" class="accordion-collapse collapse" aria-labelledby="flush-heading${index}" data-bs-parent="#accordion">
+                                     <div class="accordion-body">
+                                     ${answersHtml.join("")}
+                                     </div>
+                                </div>
+                             </div>
+                
+                              
+                                
+                        `)
     })
-
-    let validatedInput = validateInput(obj.username, obj.password);
-    if (validatedInput === false){
-        document.getElementById("username-input").classList.add("is-invalid");
-        document.getElementById("password-input").classList.add("is-invalid");
-        return ;
-    }
-
-
-    let alert = document.getElementById("alert-div");
-    if (result === true){
-        alert.classList.remove("alert-danger")
-        /*
-        alert.classList.add("alert", "alert-success");
-        alert.innerHTML = "<h3>Dobrodosli!</h3>"
-        */
-       window.location.href = './cities.html';
-    }
-    else{
-        alert.classList.remove("alert-success")
-        alert.classList.add("alert", "alert-danger");
-        alert.innerHTML = "<h3>Pogrijesili ste kredencijale.</h3>"
-    }
-        
+    accordionDiv.innerHTML = questionsHtml.join("");
 }
 
-function validateInput(username, password){
-    if (username === "" || password === "")
-        return false;
-    return true;    
+
+function finish(){
+    timeLeft = 0;
+    let points = 0;
+    questions.forEach((question, index) => {
+        let selector = `input[name=question${index}]:checked`;
+        let playerAnswer = document.querySelector(selector)?.value
+
+        if (playerAnswer === question.correctAnswer)
+            points++;
+    })
+    var resultClass = '';
+    if (points === 3)
+        resultClass = 'alert-success';
+    else if(points === 2)
+        resultClass = 'alert-warning';
+    else 
+        resultClass = 'alert-danger';
+
+    resultDiv.innerHTML = `Osvojili ste: ${points} poena`;
+    resultDiv.classList.add(resultClass);
+    tryAgainButton.classList.remove('d-none');
 }
 
+function tryAgain(){
+    window.location.reload();
+}
+
+
+start();
+
+setInterval(() => {
+    if (timeLeft == 0){
+        finish();
+        return;
+    }
+    timeLeft --;
+    timeDiv.innerHTML = `<h2>${timeLeft}</h2>`;
+}, 1000); 
